19170ff10f0c4a3d09ba204a8a783244cb3fe926
JulesBernal <102987740+JulesBernal@users.noreply.github.com>
2022-05-10 22:03:59 -0400

    nearing completion

---
+++ README.md
@@ -0,0 +1,11 @@
+Ecommerce website based on the manga and anime series, "One Piece".
+
+Included:
+Index.html -> Home Page
+shop.html -> Product Page
+contact.html -> Contact Page
+style.css -> style sheet
+script.js -> Javascript file
+
+/fruits/ -> Folder for pictures of fruits used on the webpages.
+/ships/ -> Pictures of ship and misc item, used on the webpages.
---
+++ script.js
@@ -0,0 +1,25 @@
+let slideNum=1;
+slideShow(slideNum);
+function slideMath(n){
+slideShow(slideNum+=n);
+}
+function currentSlide(n){
+slideShow(slideNum=n);
+}
+
+function slideShow(n){
+    let i;
+    let slides=document.getElementsByClassName("slideFig");
+    let tabs=document.getElementsByClassName("tabs");
+    if (slides.length<n){
+        slides.length;}
+    if (1>n){
+        slideNum=slides.length;
+    }
+    for(i=0;i<slides.length;i++){
+        slides[i].style.display="none";
+        tabs[i].className=tabs[i].className.replace(" active", "");
+    }
+    slides[slideNum-1].style.display="block";
+    tabs[slideNum-1].className+=" active";
+}
